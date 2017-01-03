81bc9e48ce8a8f9f3d5d1b6cfd9aa09942a60c92
Daniel Dimitrov <bgdppeu@gmail.com>
2017-01-03 20:13:19 +0200

    osu!API

---
+++ api/osu.py
@@ -0,0 +1,19 @@
+# osu!api
+# https://github.com/ppy/osu-api/wiki
+
+import requests
+import re
+import json
+
+with open("./config.json", "r") as f: 
+    config = json.load(f)
+
+def get_beatmap(b=None, s=None, m=0):
+    try:
+        if b != None:
+            request = requests.get("https://osu.ppy.sh/api/get_beatmaps", params={"k" : config["osu_token"], "b" : b, "m" : m})
+        else:
+            request = requests.get("https://osu.ppy.sh/api/get_beatmaps", params={"k" : config["osu_token"], "s" : s, "m" : m})
+    except requests.exceptions.RequestException as e:
+        return
+    return json.loads(request.text)
